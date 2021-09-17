6586568fe40e6a5648551934314f9ac9b95335fc
valbona1992 <valbona12@gmail.com>
2021-09-17 16:01:20 -0500

    update js files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -32,6 +32,26 @@ router.get('/', async (req, res) => {
 
 
 
+router.get('/user/:id', async (req, res) => {
+  console.log(`GET /user/:id ROUTE SLAPPED`)
+  try {
+    const profileData = await User.findByPk(req.params.id, {
+  
+    });
+
+    const user = profileData.get({ plain: true });
+    console.log(user)
+    res.render('userProfile', {
+      ...user,
+      logged_in: req.session.logged_in
+    });
+    console.log('Single user successfully loaded')
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 router.get('/events/:id', async (req, res) => {
   console.log(`GET /events/:id ROUTE SLAPPED`)
   try {
@@ -58,30 +78,18 @@ router.get('/events/:id', async (req, res) => {
 
 router.get('/searchEvents', async (req, res) => {
   console.log(`GET /searchEvents ROUTE SLAPPED`)
-  try {
-    // Get all events and JOIN with user data
-    const eventData = await Event.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['first_name', 'last_name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const events = eventData.map((event) => event.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
     res.render('searchEvents', {
-      events,
       logged_in: req.session.logged_in
-    });
-    console.log('All events successfully loaded');
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    });  
 });
+
+router.get('/userDashboard', async (req, res) => {
+  console.log(`GET /userDashboard ROUTE SLAPPED`)
+    res.render('userDashboard', {
+      logged_in: req.session.logged_in
+    });  
+});
+
 
 router.get('/createEvent', async (req, res) => {
   console.log(`GET /createEvent ROUTE SLAPPED`)
@@ -122,8 +130,8 @@ router.get('/createProfile', async (req, res) => {
   }
 });
 
-router.get('/profile', withAuth, async (req, res) => {
-  console.log(`GET /profile ROUTE SLAPPED`);
+router.get('/user', withAuth, async (req, res) => {
+  console.log(`GET /user ROUTE SLAPPED`);
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
@@ -148,7 +156,8 @@ router.get('/login', (req, res) => {
   console.log(req.body)
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+
+    res.redirect('/userDashboard');
     return;
   }
 
