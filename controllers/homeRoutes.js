706c8e40753a707c63802c561ba51765b9f1ab91
valbona1992 <valbona12@gmail.com>
2021-09-17 19:02:44 -0500

    updates to handlebars and API routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -131,9 +131,9 @@ router.get('/user', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Event }],
+      // include: [{ model: Event }],
     });
-
+    console.log("myuserdata",req.session.user_id,  userData)
     const user = userData.get({ plain: true });
 
     res.render('userProfile', {
